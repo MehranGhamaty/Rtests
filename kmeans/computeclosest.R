1b688e7b71f0fd94947a593bf8eb068e34fe64c2
Mehran Ghamaty <mehran.ghamaty@meliorix.com>
2016-08-22 20:31:15 -0700

    started gh-pages

--- kmeans/computeclosest.R
+++ kmeans/computeclosest.R
@@ -1,44 +1,37 @@
-
-
-
 centerColors = c('blue','red')
  
 #euc distance (no sqrt because our equation squares the distance)
 euc.dist <- function(x1, x2) sum((x1 - x2) ^ 2)
 
-
 #takes a set of Xs and centers and returns the center which each point is closest to
 #centers returned are from the index locations
-computeclosest <- function(X, centers,livePlot=FALSE)
+computeclosest <- function(X, centers,liveplot=FALSE,saveanimation=FALSE)
 {
-  
   #get distance from each point to each center
   Y <- NULL
   for(i in 1:nrow(X))
   {
-    if(livePlot)
+    if(liveplot)
     {
       #plot all the points that have not currently been inspected
       plot(x= X[,1], y= X[,2])
       #plot previous points with their classication color
       points(x= X[0:length(Y),1], y= X[0:length(Y),2], col=centerColors[Y],pch=16)
-      
     }
     
     #get the distance from each row 
     dist <- NULL
     for(c in 1:nrow(centers))
     {
-      if(livePlot)
+      if(liveplot)
       {
         segments(x0=X[i,1],y0=X[i,2],x1=centers[c,1],y1=centers[c,2], col='green')
       }
       dist[c] <- euc.dist(centers[c,],X[i,])
     }
-    
     closestCenter = which.min(dist)
     
-    if(livePlot)
+    if(liveplot)
     {
       #centers
       points(x=centers[,1], y=centers[,2], col=centerColors, pch=6, cex=3)
--- kmeans/kmeanstest.R
+++ kmeans/kmeanstest.R
@@ -1,27 +1,24 @@
 
-#install.packages('ggplot2', dep = TRUE)
-#install.packages('MASS', dep = TRUE)
-#install.packages('HSAUR', dep = TRUE)
 #install.packages('e1071', dep = TRUE)
-
+#install.packages("rstudioapi")
 library(datasets)
 
+#do this to set wd to current one
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-if(!exists("mykmeans", mode="function")) source("kmeans.R")
+if(!exists("mykmeans", mode="function")) source("mykmeans.R")
 if(!exists("initcentroids", mode="function")) source("initialization.R")
 
-centers <- initCentroids(k_max=3, data=iris[,2:3])
+centers <- initcentroids(k_max=3, data=iris[,2:3])
 myCluster <- mykmeans(iris[,2:3], centers)
 myCluster
 
-irisCluster <- kmeans(iris[,2:3], centers)
-irisCluster$cluster
+#irisCluster <- kmeans(iris[,2:3], centers)
+#irisCluster$cluster
 
-
-iris$Species
-
+#iris$Species
 #plot clusters
-plot(iris[,2:3], col = irisCluster$cluster)
+#plot(iris[,2:3], col = irisCluster$cluster)
 #plot with correct labels
 plot(iris[,2:3], col = iris$Species )
 
